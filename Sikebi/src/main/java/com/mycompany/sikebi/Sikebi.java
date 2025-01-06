015b35a4f02c9a388be26e007b7587bbddbe7cec
Nur Avip <cangcuter000@gmail.com>
2025-01-06 10:05:15 +0700

    Add .gitattributes to normalize line endings

---
+++ Sikebi/src/main/java/com/mycompany/sikebi/Sikebi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sikebi;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Sikebi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
